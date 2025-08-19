332e35f7d1f68d83c41fe8788c4609388f858880
Rafa <{ID}+{username}@users.noreply.github.com>
2025-08-19 13:42:45 +0200

    am I going insane

---
+++ fractol/src/main.c
@@ -0,0 +1,58 @@
+#include "./../fract-ol.h"
+#include <stdio.h>
+
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	horizontal_line(t_data *data, int start, int end, int y, int color)
+{
+	int	i;
+
+	i = start;
+	while (i <= end)
+	{
+		my_mlx_pixel_put(data, i, y++, color);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+	int		i;
+
+
+	i = 0;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	while (i <= 1080)
+	{
+		printf(" %d", i);
+		my_mlx_pixel_put(&img, i, i, i);
+		mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+		i++;
+	}
+	mlx_loop(mlx);
+}
+
---
+++ fractol/src/parse.c
@@ -0,0 +1,18 @@
+#include "../fractol.h"
+
+/*
+	available parameters:
+	1. fractal selection: M(mandelbrot) J(julia)
+	2. for julia
+void	exit_message(void)
+{
+	write(0, 
+}
+/*
+	checks that are performed:
+	- no parameters are provided: exit and message
+*/
+t_conf	parse(int argc, char *argv[])
+{
+	if 
+}
