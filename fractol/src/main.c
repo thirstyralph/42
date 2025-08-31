e33a1c9c75d7c2427e823d400f844738e41bf0a4
Rafa <{ID}+{username}@users.noreply.github.com>
2025-08-31 02:24:59 +0200

    added minimal scroll support

--- fractol/src/main.c
+++ fractol/src/main.c
@@ -1,13 +1,22 @@
 #include "../fractol.h"
 
-static void ft_error(void)
+void	my_scroll(double xdelta, double ydelta, void* param)
+{
+	(void)param;
+	(void)xdelta;
+	if (ydelta > 0)
+		write(0, "up\n", 3);
+	else if (ydelta < 0)
+		write(0, "do\n", 3);
+}
+
+static void	ft_error(void)
 {
 	exit(EXIT_FAILURE);
 }
 
 int	main(int argc, char *argv[])
 {
-
 	mlx_t	*mlx;
 	t_conf	conf;
 
@@ -17,6 +26,7 @@ int	main(int argc, char *argv[])
 		ft_error();
 	if (conf.set == 1)
 		conf.set = 2;
+	mlx_scroll_hook(mlx, my_scroll, NULL);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (0);
